dc8dd5fa8d0eaed599aba17c1700ea34f1447f89
lyw <641359145@qq.com>
2021-04-20 17:09:09 +0800

    feat(shiro集成): shiro集成

---
+++ shiro-spring-boot/src/test/java/com/example/shirospringboot/ShiroSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shirospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiroSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
